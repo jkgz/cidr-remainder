483c157e743a8e7a468bdccfa5c23907cc978738
Jason Keglovitz <jason.keglovitz@gmail.com>
2012-04-14 18:05:38 -0500

    show clean input on post

--- calc-cidr-whitelist.rb
+++ calc-cidr-whitelist.rb
@@ -16,11 +16,20 @@ def read_file(filename)
 	return IO.readlines(filename)
 end
 
+def to_array(input_string)
+	input = input_string.split("\r\n")
+	input.map! {|item| item.strip}
+	input.reject! { |item| item.empty? }
+	return input
+end
+
+def to_string(input_array)
+	return input_array.join("\r\n")
+end
+
 def to_cidr_list(input)
 	#TODO: would be better to do auto-detection of input_type, but this will do for now.
 	input_type = "cidr"
-
-	input.map! {|item| item.strip}
 
 	if input_type == "cidr"
 		cidr_list = input.map {|item| NetAddr::CIDR.create(item)} 
@@ -41,8 +50,7 @@ def calc_result(network, blacklist)
 		network = calc_remainder(network, blacklist_addr)
 	end
 
-	network = NetAddr.merge(network)
-	return network
+	return NetAddr.merge(network)
 end
 
 # TODO: the code to support input ranges for whitelist/blacklist would use NetAddr.range and then .merge that down into CIDR blocks
--- index.rb
+++ index.rb
@@ -5,18 +5,18 @@ require './calc-cidr-whitelist'
 
 
   get '/' do
-  	@network = '10.0.0.0/8'
-  	@blacklist = '10.3.0.0/16'
+  	@network = to_string(Array.[]('10.0.0.0/8', '10.3.3.32/32'))
+  	@blacklist = to_string(Array.[]('10.4.0.0/16', '10.3.0.0/16'))
     erb :index
   end
 
   post '/' do
-  	network = params['network']
-  	blacklist = params['blacklist']
+  	network = to_array(params['network'])
+  	blacklist = to_array(params['blacklist'])
 
-  	@network = network
-  	@blacklist = blacklist
-  	@result = calc_result(network.split("\r\n"), blacklist.split("\r\n"))
+  	@network = to_string(network)
+  	@blacklist = to_string(blacklist)
+  	@result = calc_result(network, blacklist)
     erb :index
   end
 
